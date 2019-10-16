8833c18cb9186f47336f7d194036ddd2bfa52c8b
junghyun <junghyun@ubuntu>
2019-10-16 16:21:15 +0900

    커밋할 변경 사항:exam2 insert

---
+++ lab2-2/ex1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "testlib.h"
+
+int main()
+{
+	printf("8 + 3 = %d\n", add(8,3));
+	printf("8 - 3 = %d\n", minus(8,3));
+	printf("8 * 3 = %d\n", multiple(8,3));
+	printf("8 / 3 = %f\n", devide(8,3));
+
+}
---
+++ lab2-2/lib/minus.c
@@ -0,0 +1,7 @@
+  /*minus.c*/
+int minus(int a, int b)
+{
+ int result;
+ result = a-b;
+ return result;
+}
